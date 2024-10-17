bd0d72f7c7e621d81e9a463799af04bc9654be7b
zied-snoussi <ziedsnoussi.tn@gmail.com>
2024-10-17 12:03:53 +0100

    Refactor App component to add Pokémon icon in the header and footer

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,16 @@ function App() {
           : "bg-gradient-to-r from-gray-100 to-gray-400 text-gray-800"
       } min-h-screen p-4`}
     >
-      <h1 className="text-4xl font-bold text-center mb-8">Pokémon List</h1>
+      <header>
+        <div className="flex flex-col items-center mb-8">
+          <img
+            src="/pokemon-icon.svg"
+            alt="Pokémon Icon representing the Pokémon list application"
+            className="w-16 h-16"
+          />
+          <h1 className="text-4xl font-bold text-center">Pokémon List</h1>
+        </div>
+      </header>
       <div
         className={`max-w-6xl mx-auto shadow-lg p-2 md:p-8 rounded-lg ${
           darkMode ? "bg-gray-800" : "bg-white"
@@ -27,6 +36,23 @@ function App() {
       >
         <PokemonList />
       </div>
+      <footer className={`text-center mt-8 ${darkMode ? "text-gray-300" : ""}`}>
+        <p>
+          Made with{" "}
+          <span role="img" aria-label="Heart">
+            ❤️
+          </span>{" "}
+          by{" "}
+          <a
+            href="https://github.com/zied-snoussi"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={`${darkMode ? "text-blue-400" : "text-blue-600"}`}
+          >
+            Zied SNOUSSI
+          </a>
+        </p>
+      </footer>
     </div>
   );
 }
