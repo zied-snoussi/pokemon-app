80f07d58f7c6b6a0ccfa52f8bbd0f0bfb4d2faf1
zied-snoussi <ziedsnoussi.tn@gmail.com>
2024-10-18 15:35:01 +0100

    Optimize code structure and improve performance

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 
 interface PaginationProps {
   totalPokemons: number;
@@ -20,21 +21,58 @@ const Pagination: React.FC<PaginationProps> = ({
 }) => {
   const pageNumbers = Math.ceil(totalPokemons / pokemonsPerPage);
 
+  const handlePrevious = () => {
+    if (currentPage > 1) {
+      paginate(currentPage - 1);
+    }
+  };
+
+  const handleNext = () => {
+    if (currentPage < pageNumbers) {
+      paginate(currentPage + 1);
+    }
+  };
+
   return (
-    <div className="pagination flex justify-center items-center mt-4">
+    <div className="pagination flex justify-center items-center mt-4 space-x-2">
+      <button
+        onClick={handlePrevious}
+        className={`p-2 border rounded-full transition duration-300 ${
+          currentPage === 1
+            ? "bg-gray-300 text-gray-500 cursor-not-allowed"
+            : "bg-gray-200 text-gray-800 hover:bg-blue-500 hover:text-white dark:bg-gray-700 dark:text-gray-200 dark:hover:bg-blue-500 dark:hover:text-white"
+        }`}
+        disabled={currentPage === 1}
+        aria-label="Previous Page"
+      >
+        <FaChevronLeft />
+      </button>
       {Array.from({ length: pageNumbers }, (_, i) => (
         <button
           key={i}
           onClick={() => paginate(i + 1)}
-          className={`p-2 border rounded mx-1 transition duration-300 ${
+          className={`p-2 border rounded-full transition duration-300 ${
             currentPage === i + 1
               ? "bg-blue-600 text-white"
               : "bg-gray-200 text-gray-800 hover:bg-blue-500 hover:text-white dark:bg-gray-700 dark:text-gray-200 dark:hover:bg-blue-500 dark:hover:text-white"
           }`}
+          aria-label={`Page ${i + 1}`}
         >
           {i + 1}
         </button>
       ))}
+      <button
+        onClick={handleNext}
+        className={`p-2 border rounded-full transition duration-300 ${
+          currentPage === pageNumbers
+            ? "bg-gray-300 text-gray-500 cursor-not-allowed"
+            : "bg-gray-200 text-gray-800 hover:bg-blue-500 hover:text-white dark:bg-gray-700 dark:text-gray-200 dark:hover:bg-blue-500 dark:hover:text-white"
+        }`}
+        disabled={currentPage === pageNumbers}
+        aria-label="Next Page"
+      >
+        <FaChevronRight />
+      </button>
     </div>
   );
 };
--- src/components/PokemonList.tsx
+++ src/components/PokemonList.tsx
@@ -5,7 +5,7 @@ import { useSelector } from "react-redux";
 import ThemeToggle from "./ThemeToggle";
 import PokemonModal from "./PokemonModal";
 import TypeDropdown from "./TypeDropdown";
-import { FaSearch } from "react-icons/fa";
+import { FaFistRaised, FaSearch, FaSort } from "react-icons/fa";
 import Loading from "./Loading";
 import { GET_POKEMONS } from "../graphql/queries";
 
@@ -49,6 +49,13 @@ const PokemonList: React.FC = () => {
   const [currentPage, setCurrentPage] = useState<number>(1);
   const [pokemonsPerPage] = useState<number>(10);
   const [selectedType, setSelectedType] = useState<string>("");
+  const [statFilter, setStatFilter] = useState<{ stat: string; value: number }>(
+    {
+      stat: "attack",
+      value: 0,
+    }
+  );
+  const [sortCriteria, setSortCriteria] = useState<string>("name");
   const [pokemonSelected, setPokemonSelected] = useState<Pokemon | null>(null);
 
   interface RootState {
@@ -60,13 +67,23 @@ const PokemonList: React.FC = () => {
   const darkMode = useSelector((state: RootState) => state.theme.darkMode);
 
   const { loading, error, data } = useQuery(GET_POKEMONS, {
-    variables: { limit: 50 }, // You can adjust the limit as needed
+    variables: { limit: 100 }, // You can adjust the limit as needed
   });
 
   const pokemons: Pokemon[] = data ? data.pokemon_v2_pokemon : [];
 
   const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSearchTerm(event.target.value.toLowerCase());
+  };
+
+  const handleStatFilterChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setStatFilter({ ...statFilter, value: parseInt(event.target.value, 10) });
+  };
+
+  const handleSortChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setSortCriteria(event.target.value);
   };
 
   const filteredPokemons = pokemons.filter((pokemon) => {
@@ -80,12 +97,36 @@ const PokemonList: React.FC = () => {
       .toLowerCase()
       .includes(searchTerm.toLowerCase());
 
-    return matchesType && matchesSearch;
+    const matchesStat =
+      statFilter.value === 0 ||
+      pokemon.pokemon_v2_pokemonstats.some(
+        (stat) =>
+          stat.pokemon_v2_stat.name === statFilter.stat &&
+          stat.base_stat > statFilter.value
+      );
+
+    return matchesType && matchesSearch && matchesStat;
+  });
+
+  const sortedPokemons = filteredPokemons.sort((a, b) => {
+    if (sortCriteria === "name") {
+      return a.name.localeCompare(b.name);
+    } else {
+      const statA =
+        a.pokemon_v2_pokemonstats.find(
+          (stat) => stat.pokemon_v2_stat.name === sortCriteria
+        )?.base_stat || 0;
+      const statB =
+        b.pokemon_v2_pokemonstats.find(
+          (stat) => stat.pokemon_v2_stat.name === sortCriteria
+        )?.base_stat || 0;
+      return statB - statA;
+    }
   });
 
   const indexOfLastPokemon = currentPage * pokemonsPerPage;
   const indexOfFirstPokemon = indexOfLastPokemon - pokemonsPerPage;
-  const currentPokemons = filteredPokemons.slice(
+  const currentPokemons = sortedPokemons.slice(
     indexOfFirstPokemon,
     indexOfLastPokemon
   );
@@ -135,11 +176,53 @@ const PokemonList: React.FC = () => {
             <ThemeToggle />
           </div>
         </div>
-        <TypeDropdown
-          selectedType={selectedType}
-          onTypeChange={setSelectedType}
-          darkMode={darkMode}
-        />
+        <div className="flex justify-between w-full items-center mb-4 left-0">
+          <TypeDropdown
+            selectedType={selectedType}
+            onTypeChange={setSelectedType}
+            darkMode={darkMode}
+          />
+          <div className="relative w-fit">
+            <FaFistRaised
+              className={`absolute left-3 top-1/2 transform -translate-y-1/2 ${
+                darkMode ? "text-white" : "text-gray-800"
+              }`}
+            />
+            <input
+              type="number"
+              placeholder="Filter by Attack Value"
+              value={statFilter.value}
+              onChange={handleStatFilterChange}
+              className={`border pl-10 p-2 rounded w-[120px] shadow-md focus:outline-none focus:ring focus:ring-blue-500 transition duration-300 ${
+                darkMode ? "bg-gray-700 text-white" : "bg-white text-gray-800"
+              }`}
+            />
+          </div>
+        </div>
+        <div className="flex justify-between w-full items-center mb-4 left-0">
+          <div className="relative w-fit">
+            <FaSort
+              className={`absolute left-3 top-1/2 transform -translate-y-1/2 ${
+                darkMode ? "text-white" : "text-gray-800"
+              }`}
+            />
+            <select
+              value={sortCriteria}
+              onChange={handleSortChange}
+              className={`border pl-10 p-2 rounded w-full shadow-md focus:outline-none focus:ring focus:ring-blue-500 transition duration-300 ${
+                darkMode ? "bg-gray-700 text-white" : "bg-white text-gray-800"
+              }`}
+            >
+              <option value="name">Sort by Name</option>
+              <option value="hp">Sort by HP</option>
+              <option value="attack">Sort by Attack</option>
+              <option value="defense">Sort by Defense</option>
+              <option value="special-attack">Sort by Special Attack</option>
+              <option value="special-defense">Sort by Special Defense</option>
+              <option value="speed">Sort by Speed</option>
+            </select>
+          </div>
+        </div>
 
         {loading ? (
           <Loading />
